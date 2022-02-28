c8cc1790ae18806021fabcafd7da01aef11a5b81
Sergei Beregov <sergei.beregov@gmail.com>
2022-02-28 14:23:28 +0200

    Remove some generated code

--- lib/r2_elixir_bench.ex
+++ lib/r2_elixir_bench.ex
@@ -1,18 +1,3 @@
 defmodule R2ElixirBench do
-  @moduledoc """
-  Documentation for `R2ElixirBench`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> R2ElixirBench.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
--- test/r2_elixir_bench_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule R2ElixirBenchTest do
-  use ExUnit.Case
-  doctest R2ElixirBench
-
-  test "greets the world" do
-    assert R2ElixirBench.hello() == :world
-  end
-end
